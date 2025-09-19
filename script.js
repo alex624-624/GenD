3a87447a841bab89009ed7394b9820a550f023a4
Alex624git <alexbenjamincohen@gmail.com>
2025-09-18 21:01:46 -0300

    Replace site with new structure

--- script.js
+++
@@ -1,7 +0,0 @@
-const WelcomeToGenD = document.getElementById('WelcomeToGenD')
-const EnterInfoButton = document.getElementById('EnterInfoButton')
-
-
-EnterInfoButton.addEventListener('click', function() {
-    window.location.href = 'Info/Info.html';
-});
